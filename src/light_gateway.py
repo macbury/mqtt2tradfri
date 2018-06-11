4652e3026427157dfbeb1fbc148ed094b6a782ff
Arkadiusz Buras <macbury@gmail.com>
2018-06-11 21:41:44 +0200

    Subscribe hybrid

--- src/light_gateway.py
+++ src/light_gateway.py
@@ -1,3 +1,4 @@
+import threading
 from pytradfri import Gateway, color
 from pytradfri.api.libcoap_api import APIFactory
 from pytradfri.error import PytradfriError, RequestTimeout
@@ -10,7 +11,7 @@ import logging
 
 logger = logging.getLogger('LightGateway')
 
-REFRESH_EVERY = 10
+REFRESH_EVERY = 60
 
 PAYLOAD_STATE = 'state'
 PAYLOAD_BRIGHTNESS = 'brightness'
@@ -48,12 +49,13 @@ class LightGateway(MqttController):
     self.actions = []
     self.config = config
     self.configure_api()
+    self.timeouts = 0
 
-    self.next_update = time.time() + 10
+    self.next_update = time.time() + REFRESH_EVERY
 
     try:
       while True:
-        self.loop_forever()
+        self.update()
     except KeyboardInterrupt:
       logger.info("Received KeyboardInterrupt")
       pass
@@ -64,16 +66,48 @@ class LightGateway(MqttController):
     self.api = api_factory.request
     self.gateway = Gateway()
     self.devices_commands = self.api(self.gateway.get_devices())
+    self.lights = []
 
   def refresh_lights(self):
-    devices = self.api(self.devices_commands)
-    self.lights = []
-    for dev in devices:
-      if dev.has_light_control:
-        logger.info("Found light: {}".format(dev.name))
-        self.lights.append(dev)
-      else:
-        logger.info("Found device: {}".format(dev.name))
+    try:
+      devices = self.api(self.devices_commands)
+      self.lights = []
+      for dev in devices:
+        if dev.has_light_control:
+          logger.info("Found light: {}".format(dev.name))
+          self.lights.append(dev)
+          self.observe(self.api, dev)
+          light_topic = set_light_topic(dev.name)
+          logger.info('Subscribing to {}'.format(light_topic))
+          self.client.unsubscribe(light_topic)
+          self.client.subscribe(light_topic)
+        else:
+          logger.info("Found device: {}".format(dev.name))
+      self.read_light_states()
+    except RequestTimeout as e:
+      logger.info("There was timeout for fetching lights, retry in some time")
+      self.next_update = time.time() + 1
+      self.timeouts = self.timeouts + 1
+
+  def observe(self, api, device):
+    logger.info("Registering observe for light: {}".format(device.name))
+    def callback(updated_device):
+      logger.info("On observe for light: {}".format(updated_device.name))
+      self.push_light_state(updated_device)
+
+    def err_callback(err):
+      logger.error("On observe callback error")
+      logger.error(err, exc_info=True)
+
+    def worker():
+      try:
+        logger.info("Started Worker observe for light: {}".format(device.name))
+        api(device.observe(callback, err_callback, duration=REFRESH_EVERY))
+        logger.info("Worker ended lifecycle for {}".format(device.name))
+      except Exception as err:
+        logger.error("On observe callback error")
+        logger.error(err, exc_info=True)
+    threading.Thread(target=worker, daemon=True).start()
 
   def find_light(self, name):
     for light in self.lights:
@@ -82,12 +116,7 @@ class LightGateway(MqttController):
 
   def on_connect(self, client, userdata, flags, rc):
     logger.info('Connected to broker')
-    self.refresh_lights()
-    for light in self.lights:
-      logger.info('Subscribing to {}'.format(set_light_topic(light.name)))
-      self.client.subscribe(set_light_topic(light.name))
-
-    self.next_update = time.time()
+    self.next_update = time.time() + 5
 
   def on_message(self, client, userdata, msg):
     logger.info('Received message: {} with payload: {}'.format(msg.topic, msg.payload))
@@ -136,22 +165,38 @@ class LightGateway(MqttController):
     logger.info("Publishing to {} with payload {}".format(topic, payload))
     self.client.publish(topic, payload, 1)
 
+  def restart_gateway(self):
+    logger.info("Restaring gateway")
+    self.timeouts = 0
+    topic = "home/living_room/rf_emitter/set"
+    self.client.topic(topic, json.dumps({ "name": "ikea_gateway", "code": 1364, "pulse_length": 321 }), 1)
+    logger.info("Switched off")
+    time.sleep(10)
+    logger.info("Switched on")
+    self.client.topic(topic, json.dumps({ "name": "ikea_gateway", "code": 1361, "pulse_length": 321 }), 1)
+    time.sleep(30)
+    self.refresh_lights()
+
   def process_actions(self):
     if len(self.actions) == 0:
       return
-    logger.info("There is {} actions to execute".format(len(self.actions)))
-    self.api(self.actions)
-    self.actions = []
-    self.next_update = time.time() + 0.1
-
-  def loop_forever(self):
     try:
-      self.process_actions()
-      if self.should_update_lights():
-        self.refresh_lights()
-        self.read_light_states()
-      self.client.loop(timeout=0.01)
-    except Exception as e:
-      self.configure_api()
-      logger.error('Loop exception!')
-      logger.error(e, exc_info=True)
+      logger.info("There is {} actions to execute".format(len(self.actions)))
+      self.api(self.actions)
+      self.actions = []
+      logger.info("Pushed action!")
+      self.read_light_states()
+    except RequestTimeout as e:
+      logger.info("There was timeout for actions, retry in some time")
+      self.timeouts = self.timeouts + 1
+
+  def update(self):
+    self.client.loop(timeout = 0.5)
+    self.process_actions()
+    if self.should_update_lights():
+      self.refresh_lights()
+      self.read_light_states()
+      self.timeouts = 0
+    time.sleep(0.5)
+    if self.timeouts >= 3:
+      self.restart_gateway()
